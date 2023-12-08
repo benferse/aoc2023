bb9ef5c92af23cdf7eae86a2900a4972290ec4b9
Ben Fersenheim <benferse@microsoft.com>
2023-12-08 10:05:35 -0500

    Prettier

--- src/day8.rs
+++ src/day8.rs
@@ -10,10 +10,8 @@ pub struct Map<'a> {
 
 impl<'a> Map<'a> {
     pub fn parse(input: &'a str) -> Self {
+        // First line is the set of instructions, rest are node mappings
         let (instructions, rest) = input.split_once("\n\n").expect("Malformed");
-
-        // First line is the set of instructions
-        // The rest are node mappings
         let re = Regex::new(r"(\w\w\w) = \((\w\w\w), (\w\w\w)\)").expect("Should have compiled");
         let nodes = re.captures_iter(rest).map(|c| {
             let (_, [start, left, right]) = c.extract();
@@ -34,11 +32,11 @@ impl<'a> Map<'a> {
             }
 
             if let Some(node) = self.nodes.get(current) {
-            match x {
-                'L' => current = node.0,
-                'R' => current = node.1,
-                _ => unimplemented!(),
-            }
+                match x {
+                    'L' => current = node.0,
+                    'R' => current = node.1,
+                    _ => unimplemented!(),
+                }
             }
 
             steps += 1;
@@ -52,21 +50,6 @@ impl<'a> Map<'a> {
     }
 }
 
-pub fn parse(input: &str) -> (&str, HashMap<&str, (&str, &str)>) {
-    let (first, rest) = input.split_once("\n\n").expect("Malformed");
-
-    // First line is the set of instructions
-    // The rest are node mappings
-    let re = Regex::new(r"(\w\w\w) = \((\w\w\w), (\w\w\w)\)").expect("Should have compiled");
-    let nodes = re.captures_iter(rest).map(|c| {
-        let (_, [start, left, right]) = c.extract();
-        (start, (left, right))
-    })
-        .collect();
-
-    (first, nodes)
-}
-
 #[cfg(test)]
 mod answers {
     use super::*;
@@ -76,7 +59,6 @@ mod answers {
     const SAMPLE_INPUT_2: &str = include_str!("./input/day8-sample-2.txt");
     const SAMPLE_INPUT_3: &str = include_str!("./input/day8-sample-3.txt");
     const PERSONAL_INPUT: &str = include_str!("./input/day8-real.txt");
-
 
     #[test_case(SAMPLE_INPUT_1 => 2; "with first sample data")]
     #[test_case(SAMPLE_INPUT_2 => 6; "with second sample data")]
@@ -89,11 +71,8 @@ mod answers {
     #[test_case(PERSONAL_INPUT => 12357789728873; "with personal data")]
     pub fn problem2(input: &str) -> u64 {
         let map = Map::parse(input);
-
         map.part2_nodes()
-            .map(|&start| {
-                map.count_steps_until(start, |x| x.ends_with('Z'))
-            })
+            .map(|&start| { map.count_steps_until(start, |x| x.ends_with('Z')) })
             .reduce(num::integer::lcm)
             .expect("Well that's just bad math")
     }
